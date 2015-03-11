0fefd23f191b1099c55a65254812b3e9651d38d5
Tom Mertz & Cynthia Wilhelm <student@epicodus.com>
2015-03-11 13:55:50 -0700

    Add code to app.php routes to process form data and determine winner

--- app/app.php
+++ app/app.php
@@ -3,8 +3,8 @@
     require_once __DIR__.'/../src/RockPaperScissors.php';
 
     session_start();
-    if(empty($_SESSION['list_of_choices'])) {
-        $_SESSION['list_of_choices'] = array();
+    if(empty($_SESSION['player1'])) {
+        $_SESSION['player1'] = array();
     }
 
     $app = new Silex\Application();
@@ -12,17 +12,21 @@
 
     $app->get('/', function () use ($app) {
 
-    return $app['twig']->render('form.twig', array('col1' => 'selected', 'col2' => 'grey', 'display' => 'player1'));
+        return $app['twig']->render('form.twig', array('col1' => 'selected', 'col2' => 'grey', 'display' => 'player1'));
     });
 
     $app->post('/player2', function() use ($app) {
+
+        $_SESSION['player1'][0] = $_POST['player1'];
 
         return $app['twig']->render('form.twig', array('col1' => 'grey', 'col2' => 'selected', 'display' => 'player2'));
     });
 
     $app->post('/outcome', function() use ($app) {
+        $outcome = new RockPaperScissors();
+        $winner = $outcome->gameOutcome($_SESSION['player1'][0], $_POST['player2']);
 
-        return $app['twig']->render('outcome.twig');
+        return $app['twig']->render('outcome.twig', array('winner' => $winner, 'player1' => $_SESSION['player1'][0], 'player2' => $_POST['player2']));
     });
 
     return $app;
