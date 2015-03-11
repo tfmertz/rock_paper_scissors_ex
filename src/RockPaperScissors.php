898561d3bc5c6d061cae899ade6ca408eb4dff37
Tom Mertz & Cynthia Wilhelm <student@epicodus.com>
2015-03-11 10:44:09 -0700

    Add PHPUnit tests for all game scenarios and possibility of inconsistent input string case

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -4,8 +4,19 @@
 	{
 		function gameOutcome($player1, $player2)
 		{
-			if ($player1 == $player2) {
+			$picks = array(strtolower($player1), strtolower($player2));
+			if ($picks[0] == $picks[1]) {
 				return 'Tie';
+			}
+			if(in_array("rock", $picks) && in_array("paper", $picks)) {
+				$string = "Player " . (array_search("paper", $picks) + 1);
+				return $string;
+			} else if (in_array("rock", $picks) && in_array("scissors", $picks)) {
+				$string = "Player " . (array_search("rock", $picks) + 1);
+				return $string;
+			} else {
+				$string = "Player " . (array_search("scissors", $picks) + 1);
+				return $string;
 			}
 		}
 	}
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -24,7 +24,7 @@
 
         /*
         Input: “Scissors”, “Scissors”
-        Output: Tie
+        Output: "Tie"
         Spec: Both players play Scissors; game ends in tie.
         */
         function test_gameOutcome_scissorsScissors()
@@ -40,7 +40,7 @@
 
         /*
         input: “Paper”, “Paper”
-        Output: Tie
+        Output: "Tie"
         Spec: Both players play Paper; game ends in tie.
         */
         function test_gameOutcome_paperPaper()
@@ -53,6 +53,89 @@
 
             $this->assertEquals("Tie", $result);
         }
+
+        /*
+        input: “Rock”, “Paper”
+        output: “Player 2”
+        Spec: One player chooses Rock, another chooses Paper, player 2 wins
+        */
+        function test_gameOutcome_rockPaper()
+        {
+            $test_RockPaperScissors = new RockPaperScissors();
+            $input1 = "Rock";
+            $input2 = "Paper";
+
+            $result = $test_RockPaperScissors->gameOutcome($input1, $input2);
+
+            $this->assertEquals("Player 2", $result);
+        }
+
+        /*
+        input: “Paper”, “Rock”
+        output: “Player 1"
+        Spec: One player chooses Paper, another chooses Rock, player 1 wins
+        */
+
+        function test_gameOutcome_paperRock()
+        {
+            $test_RockPaperScissors = new RockPaperScissors();
+            $input1 = "Paper";
+            $input2 = "Rock";
+
+            $result = $test_RockPaperScissors->gameOutcome($input1, $input2);
+
+            $this->assertEquals("Player 1", $result);
+        }
+
+        /*
+        input: “Rock”, “Scissors”
+        output: “Player 1”
+        Spec: One player plays ROCK, other plays Scissors; Player 1 wins.
+        */
+        function test_gameOutcome_rockScissors()
+        {
+            $test_RockPaperScissors = new RockPaperScissors();
+            $input1 = "Rock";
+            $input2 = "Scissors";
+
+            $result = $test_RockPaperScissors->gameOutcome($input1, $input2);
+
+            $this->assertEquals("Player 1", $result);
+        }
+
+        /*
+        input: “Scissors”, “Rock”
+        output: “Player 2”
+        Spec: One player plays Scissors, other plays Rock; Player 2 wins.
+        */
+
+        function test_gameOutcome_scissorsRock()
+        {
+            $test_RockPaperScissors = new RockPaperScissors();
+            $input1 = "Scissors";
+            $input2 = "Rock";
+
+            $result = $test_RockPaperScissors->gameOutcome($input1, $input2);
+
+            $this->assertEquals("Player 2", $result);
+        }
+
+        /*
+        input: “ROcK”, “sciSsOrs”
+        Output: “Player 1”
+        Spec: players enter letters in a mix of upcase and downcase, return the correct winner
+        */
+        function test_gameOutcome_upcase()
+        {
+            $test_RockPaperScissors = new RockPaperScissors();
+            $input1 = "ROcK";
+            $input2 = "sciSsOrs";
+
+            $result = $test_RockPaperScissors->gameOutcome($input1, $input2);
+
+            $this->assertEquals("Player 1", $result);
+        }
+
 
     }
 
